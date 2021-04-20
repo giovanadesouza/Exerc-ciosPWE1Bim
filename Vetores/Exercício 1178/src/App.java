b596f29f86c585cff9267bccc35dc12f1573101b
Your Name <you@example.com>
2021-04-20 11:38:38 -0300

    Enviando

--- Vetores/Exercício 1178/src/App.java
+++ Vetores/Exercício 1178/src/App.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        Scanner leitor = new Scanner(System.in);
-        double X = leitor.nextDouble();
+        System.out.println("Digite um valor:");
+        Scanner funcionapf = new Scanner(System.in);
+        double X = funcionapf.nextDouble();
         double[] N = new double[100];
         N[0] = X;
         
@@ -12,6 +13,7 @@ public class App {
         {
         	N[i] = N[i - 1] / 2.0000;
         }
+
         for (int i = 0; i < N.length; i++) 
         {
         	System.out.println(String.format("N[" + i + "] = %.4f", N[i]));
